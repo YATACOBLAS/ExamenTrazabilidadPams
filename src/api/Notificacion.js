b90e1e0b003590f8fff557c1a4cf2f0276ce1921
YATACOBLAS <JUAN_BLAS_1905@HOTMAIL.COM>
2022-10-26 20:46:00 -0500

    code redundante dotenv

--- src/api/Notificacion.js
+++ src/api/Notificacion.js
@@ -1,21 +1,20 @@
 const apiNotificacion = {};
 
 require('dotenv').config();
-const client = require('twilio')('', ''); 
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID , process.env.TWILIO_TOKEN); 
  
 apiNotificacion.notificar= (req,res)=>{
     let nombre=req.body.nombre;
     let examen=req.body.examen;
     let telefono=req.body.telefono;
-   
-
+   console.log(process.env.TWILIO_ACCOUNT_SID )
    client.messages 
    .create({ 
       body: `Hola *${nombre}*🙂, el resultado del examen 🧪 *${examen}* que te realizaste en el policlinico PAMS 👨‍⚕ esta listo, puede acercarse hoy mismo.`,   
-      messagingServiceSid: '',      
+      messagingServiceSid: process.env.MESSAGIND_SERVICE_SID,      
       to: `+51957834644` 
-    }) 
-   .then(message => {
+    })
+    .then(message => {
     res.json({mensaje:"Notificacion Enviada"});
    }).catch(e =>{
         res.status(400).json({mensaje:"Error al Notificar"}) 
